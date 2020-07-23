4bc81844d8b7cf1f8b2b263d37106ded8a5397cd
huakaimay <huakaimay@gmail.com>
2020-07-23 11:04:04 +0800

    udpate

---
+++ Code/src/main/java/core/java/chapter3/C3_6_6.java
@@ -0,0 +1,29 @@
+package core.java.chapter3;
+
+import java.util.Arrays;
+
+/**
+ * @author: huakaimay
+ * @since: 2020-07-23
+ */
+public class C3_6_6 {
+
+    public static void main(String[] args) {
+        String s = "ABC";
+        System.out.println("第i个码点：");
+        int index = s.offsetByCodePoints(0, 2);
+        int cp = s.codePointAt(index);
+        System.out.println(cp);
+
+        System.out.println("字符串转为码点：");
+        int[] ints = s.codePoints().toArray();
+        for (int anInt : ints) {
+            System.out.println(anInt);
+        }
+
+        System.out.println("码点转为字符串：");
+        String s1 = new String(ints, 0, ints.length);
+        System.out.println(s1);
+
+    }
+}
