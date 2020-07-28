f1c3268c67c96e97f9e98b2fee18a1d625e42588
huakaimay <huakaimay@gmail.com>
2020-07-28 13:34:45 +0800

    4.2.2

--- Code/src/main/java/core/java/chapter3/C4_2_2.java
+++ Code/src/main/java/core/java/chapter3/C4_2_2.java
@@ -1,6 +1,7 @@
 package core.java.chapter3;
 
 import java.text.SimpleDateFormat;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -15,9 +16,19 @@ public class C4_2_2 {
     public static void main(String[] args) {
 
         LocalDateTime now = LocalDateTime.now();
+        /*
+        格式化日期
+         */
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         String format = now.format(dateTimeFormatter);
         System.out.println(format);
+
+        /*
+         给日期加上指定天数
+         */
+        LocalDate localDate = LocalDate.of(2019, 11, 3).plusDays(365);
+        System.out.println(localDate);
+
 
     }
 }
