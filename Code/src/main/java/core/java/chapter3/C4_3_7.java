8a553c0862d0fbfd3fac2c45450b219ce0fb2a2a
huakaimay <huakaimay@gmail.com>
2020-07-30 13:24:01 +0800

    4.3.7

---
+++ Code/src/main/java/core/java/chapter3/C4_3_7.java
@@ -0,0 +1,15 @@
+package core.java.chapter3;
+
+/**
+ * @author: huakaimay
+ * @since: 2020-07-30
+ */
+public class C4_3_7 {
+
+    private String name;
+
+    public void tesst() {
+        System.out.println(name);
+    }
+
+}
