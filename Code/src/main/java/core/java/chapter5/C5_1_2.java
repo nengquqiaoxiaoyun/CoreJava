853e3a07ae67602d8e4d2233e547357e639d2e67
huakaimay <huakaimay@gmail.com>
2020-07-31 13:54:09 +0800

    5.1.2

---
+++ Code/src/main/java/core/java/chapter5/C5_1_2.java
@@ -0,0 +1,12 @@
+package core.java.chapter5;
+
+/**
+ * @author: huakaimay
+ * @since: 2020-07-31
+ */
+public class C5_1_2 extends Employee {
+
+    public void aa() {
+        String name = super.getName();
+    }
+}
---
+++ Code/src/main/java/core/java/chapter5/Employee.java
@@ -0,0 +1,13 @@
+package core.java.chapter5;
+
+/**
+ * @author: huakaimay
+ * @since: 2020-07-31
+ */
+public class Employee {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+}
