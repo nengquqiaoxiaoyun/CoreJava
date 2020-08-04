6e8d006a7fd4eadea67cd718572b85298fa7f479
huakaimay <huakaimay@gmail.com>
2020-08-04 10:42:47 +0800

    manager

---
+++ Code/src/main/java/core/java/chapter5/Manager.java
@@ -0,0 +1,25 @@
+package core.java.chapter5;
+
+/**
+ * @author: huakaimay
+ * @since: 2020-08-04
+ */
+public  class Manager extends Employee {
+
+    private double bonus;
+
+    public Manager(String name, double salary, int year, int month, int day) {
+        super(name, salary, year, month, day);
+        bonus = 0;
+    }
+
+    public double getSalary() {
+        double baseSalary = super.getSalary();
+        return baseSalary + bonus;
+    }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+
+}
