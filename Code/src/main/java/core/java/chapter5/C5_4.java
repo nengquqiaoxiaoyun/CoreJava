00a2ba5fc195056e8d8d50dcf4f2104fbf23d433
huakaimay <huakaimay@gmail.com>
2020-08-18 16:17:27 +0800

    5.4

---
+++ Code/src/main/java/core/java/chapter5/C5_4.java
@@ -0,0 +1,23 @@
+package core.java.chapter5;
+
+/**
+ * @author: huakaimay
+ * @since: 2020-08-18
+ */
+public class C5_4 {
+
+    public static void main(String[] args) {
+        Integer a = 1000;
+        Integer b = 1000;
+        System.out.println(a == b);
+
+        Short s1 = 128;
+        Short s2 = 128;
+        System.out.println(s1 == s2);
+
+        Byte b1 = 127;
+        Byte b2 = 127;
+        System.out.println(b1 == b2);
+    }
+
+}
