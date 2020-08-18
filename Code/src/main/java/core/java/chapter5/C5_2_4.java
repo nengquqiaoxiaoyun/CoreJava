b3efed423a9d7183022104901434cafb2a7eaad9
huakaimay <huakaimay@gmail.com>
2020-08-18 16:17:04 +0800

    5.2.4

---
+++ Code/src/main/java/core/java/chapter5/C5_2_4.java
@@ -0,0 +1,23 @@
+package core.java.chapter5;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * @author: huakaimay
+ * @since: 2020-08-18
+ */
+public class C5_2_4 {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
+        String s = "" + arr;
+        System.out.println(s);
+
+        String s1 = Arrays.toString(arr);
+        System.out.println(s1);
+
+
+    }
+
+}
