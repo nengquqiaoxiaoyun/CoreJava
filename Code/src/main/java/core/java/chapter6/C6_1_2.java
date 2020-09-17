214bdeb2d2382f1fc1d12bedc480dbdfd800ca80
huakaimay <huakaimay@gmail.com>
2020-09-17 09:08:14 +0800

    update

--- Code/src/main/java/core/java/chapter6/C6_1_2.java
+++ Code/src/main/java/core/java/chapter6/C6_1_2.java
@@ -4,9 +4,9 @@ package core.java.chapter6;
  * @author: huakaimay
  * @since: 2020-09-04
  */
-public interface C6_1_2 {
+public  interface C6_1_2 {
 
-    void a();
+     void a();
 
     default void c() {
         System.out.println("sdfa");
