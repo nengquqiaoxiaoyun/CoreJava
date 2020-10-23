30a0fd46cfcdf9df0de3ece8ef9727d9cf322fbf
huakaimay <huakaimay@gmail.com>
2020-10-23 10:57:33 +0800

    6.4.6 匿名内部类

---
+++ Code/src/main/java/core/java/chapter6/c646/Sub.java
@@ -0,0 +1,15 @@
+package core.java.chapter6.c646;
+
+/**
+ * @author: huakaimay
+ * @since: 2020-10-23
+ */
+public class Sub extends Super {
+
+
+    public Sub(int id) {
+        super(id);
+    }
+
+    
+}
---
+++ Code/src/main/java/core/java/chapter6/c646/Super.java
@@ -0,0 +1,24 @@
+package core.java.chapter6.c646;
+
+/**
+ * @author: huakaimay
+ * @since: 2020-10-23
+ */
+public class Super {
+
+    private int id;
+
+    public Super(int id) {
+        this.id = id;
+    }
+
+    public static void main(String[] args) {
+        int id = 1;
+        /*
+        匿名类不能有构造器，必须将构造器参数传递给父类构造器
+         */
+        Super superClass = new Super(id) {
+
+        };
+    }
+}
