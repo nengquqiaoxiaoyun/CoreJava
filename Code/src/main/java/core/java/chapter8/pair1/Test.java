949dbbf0fa5b5f9666823c3f58cbed91d1846351
huakaimay <huakaimay@gmail.com>
2020-10-29 10:11:57 +0800

    test

---
+++ Code/src/main/java/core/java/chapter8/pair1/Test.java
@@ -0,0 +1,19 @@
+package core.java.chapter8.pair1;
+
+import java.util.LinkedList;
+
+/**
+ * @author: huakaimay
+ * @since: 2020-10-29
+ */
+public class Test {
+    public static void main(String[] args) {
+        Class c1 = new LinkedList<String>().getClass();
+        Class c2 = new LinkedList<Double>().getClass();
+
+        
+
+        System.out.println(c1 == c2);
+        System.out.println(c1.getName());
+    }
+}
